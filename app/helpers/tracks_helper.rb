61067009a9043c03b46780b13aabea49cea2a5f0
koullouros <r.koullouros@gmail.com>
2020-11-22 19:11:38 +0000

    Added average rating in track view and seeds

--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -1,2 +1,12 @@
 module TracksHelper
+  def avg_rating(track)
+    total = 0
+    n = 0
+    track.reviews.each do |r|
+      total += r.rating
+      n += 1
+    end
+
+    return total/n
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Review.delete_all
 Track.delete_all
 Album.delete_all
 a1 = Album.create(title: 'Test album 1', artist: 'Test artist 1', released: Date.new(1999,5,1))
@@ -18,3 +19,7 @@ t3 = Track.create(title: 'Test track 3', artist: 'Test artist 1', released: Date
 t4 = Track.create(title: 'Test track 1', artist: 'Test artist 2', released: Date.new(1994,2,5), minutes: 1, seconds: 5, album: a2)
 t5 = Track.create(title: 'Test track 2', artist: 'Test artist 2', released: Date.new(1994,2,5), minutes: 2, seconds: 5, album: a2)
 t6 = Track.create(title: 'Test track 3', artist: 'Test artist 2 and Test artist 3', released: Date.new(2005,1,5), minutes: 2, seconds: 30, album: a2)
+
+
+r1 = Review.create(name: 'reviewer 1', rating: 3, description: 'ok, i like the drop', track: t1)
+r2 = Review.create(name: 'reviewer 2', rating: 1, description: 'I dont like it', track: t1)
