cf59754e1a74f9d37cbfac106d6d5d5fbe02a6bb
koullouros <r.koullouros@gmail.com>
2020-11-26 16:50:31 +0000

    fixed a bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  # calculates the average rating for a track
+  def avg_rating(track)
+    total = 0
+    n = 0
+    track.reviews.each do |r|
+      total += r.rating
+      n += 1
+    end
+
+    if n != 0
+      return total/n
+    else
+      return 0
+    end
+  end
 end
--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -1,12 +1,3 @@
 module TracksHelper
-  def avg_rating(track)
-    total = 0
-    n = 0
-    track.reviews.each do |r|
-      total += r.rating
-      n += 1
-    end
 
-    return total/n
-  end
 end
